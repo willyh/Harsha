3ff6564c430c5d459737ac22810b60778dee925c
Willy Hauser <WillyH258@gmail.com>
2011-07-11 08:55:41 -0400

    updated regex again replaced . with [.]

--- app/helpers/menu_items_helper.rb
+++ app/helpers/menu_items_helper.rb
@@ -2,7 +2,7 @@ module MenuItemsHelper
   def format_price price
     if price <= 0
       "free"
-    elsif price.to_s =~ /\d*.\d{2,}/
+    elsif price.to_s =~ /\d*[.]\d{2,}/
       "$#{price}"
     else
       "$#{price}0"
