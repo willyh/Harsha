2c6049c2df55e36ca8ed67bf1ee5260f006727a8
Willy Hauser <WillyH258@gmail.com>
2011-07-27 15:16:24 -0400

    removed old files from git

--- db/migrate/20110714184838_add_pickup_time_uniqueness_index.rb
+++
@@ -1,9 +0,0 @@
-class AddPickupTimeUniquenessIndex < ActiveRecord::Migration
-  def self.up
-    add_index :orders, :pickup_time, :unique => true
-  end
-
-  def self.down
-    remove_index :orders, :pickup_time
-  end
-end
