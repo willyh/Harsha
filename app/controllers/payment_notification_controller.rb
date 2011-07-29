4c2519d0e6a5e0cd9e2f45a64285e8e78e0fb686
Willy Hauser <WillyH258@gmail.com>
2011-07-29 16:40:29 -0400

    detail of notification menu item now displays params

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -2,8 +2,8 @@ class PaymentNotificationController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def create
-    MenuItem.create!(:name => "Paypal made this", :price => 0, :category => "payment_notification", :description => (params[:invoice] || "no params"))
     PaymentNotification.create!(:params => params, :order_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
+    MenuItem.create!(:name => "Paypal made this", :price => 0, :category => "payment_notification", :description => "Order id is #{params[:invoice]}")
   end
 
 end
