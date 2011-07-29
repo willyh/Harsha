1183d789b3026358916c9462772f69dc15aade76
Willy Hauser <WillyH258@gmail.com>
2011-07-29 15:59:36 -0400

    typo

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -2,7 +2,7 @@ class PaymentNotificationController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def create
-    MenuItem.create!(:name => "Paypal made this", :price => 0, :category => "payment_notification"
+    MenuItem.create!(:name => "Paypal made this", :price => 0, :category => "payment_notification")
     PaymentNotification.create!(:params => params, :order_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
   end
 
