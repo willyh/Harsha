701ab10eb8faa3b4a2bc24e7e10b6ba3de0342db
Willy Hauser <WillyH258@gmail.com>
2011-07-29 17:08:15 -0400

    paypal works

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -2,8 +2,7 @@ class PaymentNotificationController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def create
-    MenuItem.create!(:name => "Paypal made this", :price => 0, :category => "payment_notification", :description => "Order id is #{params[:invoice]}\n status is #{params[:payment_status]}\n transaction id is #{params[:txn_id]}")
-    PaymentNotification.create!( :order_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
+    PaymentNotification.create!( :params => params, :order_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
   end
 
 end
