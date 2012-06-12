576ff1255b712e80cc92987f347e7756d757196e
Willy Hauser <willyh258@gmail.com>
2012-06-12 16:03:23 -0400

    changes in edit menu - can now add new item and choose category

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,5 @@
 class MenuItemsController < ApplicationController
-  before_filter :authorize, :except =>  [:index, :show, :home]
+  before_filter :authorize, :except =>  [:show, :home]
   def index
     @menu_items = MenuItem.all.sort{ |x,y| x.id<=>y.id }
     @menu_item = MenuItem.new 
@@ -12,9 +12,21 @@ class MenuItemsController < ApplicationController
     @menu_item = MenuItem.new(attrs)
     @menu_item.category = @menu_item.category.capitalize || "Other"
     if @menu_item.save
-      redirect_to menu_path :category => @menu_item.category
+      redirect_to menu_path
     else
       @menu_items = MenuItem.all.sort{ |x,y| x.id<=>y.id }
+      @head = "Error"
+      render 'index'
+    end
+  end
+
+  def update
+    @menu_item = MenuItem.find(params[:id])
+    if @menu_item.update_attributes(params[:menu_item])
+      redirect_to menu_path
+    else
+      @menu_items = MenuItem.all.sort{ |x,y| x.id<=>y.id }
+      @head = "Error"
       render 'index'
     end
   end
@@ -30,20 +42,10 @@ class MenuItemsController < ApplicationController
     @head = "Edit #{@menu_item.name}"
   end
 
-  def update
-    @menu_item = MenuItem.find(params[:id])
-    if @menu_item.update_attributes(params[:menu_item])
-      redirect_to menu_path :category => @menu_item.category
-    else
-      @menu_items = MenuItem.all.sort{ |x,y| x.id<=>y.id }
-      render 'edit'
-    end
-  end
-
   def destroy
     category = MenuItem.find(params[:id]).category
     MenuItem.find(params[:id]).destroy
-    redirect_to menu_path :category => category
+    redirect_to menu_path
   end
 
   def home
@@ -54,6 +56,6 @@ class MenuItemsController < ApplicationController
     @order = MenuItem.find(params[:id])
     @order.sold_out
     @order.save
-    redirect_to menu_items_path :category => @order.category
+    redirect_to menu_path
   end
 end
