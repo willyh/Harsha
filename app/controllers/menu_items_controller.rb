c59140d22051db25c7fcbf77787091b404a33d1c
Willy Hauser <willyh258@gmail.com>
2012-06-14 17:26:04 -0400

    Show first menu item when entering menu and still give option to create new item on error page for update

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,24 +1,29 @@
 class MenuItemsController < ApplicationController
   before_filter :authorize, :except =>  [:show, :home]
   def index
-    @menu_item = MenuItem.new 
+    @new_item = MenuItem.new
+    @menu_item = @new_item
     @head = "Check Out Your Menu"
   end
 
   def create
+    #@menu_item and @new_item will be the same unless returning from
+    #a failed update then new_item will be new and
+    #menu_item will be the item that failed to update
     attrs = params[:menu_item]
-    attrs[:category]= params[:new_category] if attrs[:category]=="new_category"
-    @menu_item = MenuItem.new(attrs)
-    @menu_item.category = @menu_item.category.capitalize || "Other"
-    if @menu_item.save
+    @new_item = MenuItem.new(attrs)
+    @new_item.category = @new_item.category.capitalize || "Other"
+    if @new_item.save
       redirect_to menu_path
     else
+      @menu_item = @new_item
       @head = "Error"
       render 'index'
     end
   end
 
   def update
+    @new_item = MenuItem.new
     @menu_item = MenuItem.find(params[:id])
     if @menu_item.update_attributes(params[:menu_item])
       redirect_to menu_path
@@ -35,6 +40,7 @@ class MenuItemsController < ApplicationController
 
   def edit
     redirect_to menu_path
+    @new_item = MenuItem.new
     @menu_item = MenuItem.find(params[:id])
     @head = "Check Out Your Menu!"
   end
